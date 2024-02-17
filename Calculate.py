9067203e7a8f10d3ae1e42a2853b90aa8a1a69ee
Swischya Sunthonphusit <swischya.s@ku.th>
2024-02-17 17:13:01 +0700

    commit calculator

---
+++ Calculate.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from math import *
+
+# class Calculate(CalculatorUI):
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.get_entry = self.entry.get()
+#
+#     def calculate(self):
+#         self.show(eval(str(self.get_entry)))
